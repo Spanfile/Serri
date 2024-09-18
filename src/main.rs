dd95a7e91998d06bf96bfc54cb3cfe7f2db41977
Spans <spansdev@gmail.com>
2024-09-18 12:29:54 +0300

    Don't log debug info for release builds

--- src/main.rs
+++ src/main.rs
@@ -28,11 +28,15 @@ async fn main() -> anyhow::Result<()> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!(
-                    "{}=trace,tower_http=debug,axum=trace",
-                    env!("CARGO_CRATE_NAME")
-                )
-                .into()
+                if cfg!(debug_assertions) {
+                    format!(
+                        "{}=trace,tower_http=debug,axum=trace",
+                        env!("CARGO_CRATE_NAME")
+                    )
+                    .into()
+                } else {
+                    "info".into()
+                }
             }),
         )
         .with(tracing_subscriber::fmt::layer().without_time())
